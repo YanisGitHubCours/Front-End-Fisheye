7f144d53c7abfa1c3056115d4e9d248f47bf187f
Yanis <ytagri1@myges.fr>
2023-04-20 22:27:58 +0200

    some content

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,5 +1,5 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -7,10 +7,20 @@ function photographerFactory(data) {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+        img.addEventListener("click",()=>{window.location.href="photographer.html"})
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const p = document.createElement('p')
+        p.textContent = city + ", " + country;
+        const mytagline = document.createElement('p')
+        mytagline.textContent = tagline
+        const prices = document.createElement('p')
+        prices.textContent = price + "/jour"
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(p)
+        article.appendChild(mytagline)
+        article.appendChild(prices)
         return (article);
     }
     return { name, picture, getUserCardDOM }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,29 +1,19 @@
     async function getPhotographers() {
+        let photographers = []
         // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
         // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
+        await fetch('./data/photographers.json').then(response => {
+            return response.json();
+          }).then(data => {
+            // Work with JSON data here
+            console.log(data)
+            photographers = data["photographers"]
+            
+          }).catch(err => {
+            console.log(err);
+          });
         // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+        return photographers
     }
 
     async function displayData(photographers) {
@@ -38,7 +28,7 @@
 
     async function init() {
         // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
+        const photographers = await getPhotographers();
         displayData(photographers);
     };
     
